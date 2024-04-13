2f690a70c7c3814b22d320681cb04f689c6ebfc8
salonimore11 <149373203+salonimore11@users.noreply.github.com>
2024-04-13 14:44:58 +0530

    Create student-app.groovy

---
+++ student-app.groovy
@@ -0,0 +1,37 @@
+pipeline{
+    agent any
+
+    stages {
+        stage('pull') {
+            steps {
+                git branch: 'main', changelog: false, credentialsId: 'ssh-key', poll:false, url: 'https://github.com/chetansomkuwar254/jenkins-b1.git'
+                echo 'data pulling is done'
+            }
+        }
+        stage('build') {
+            steps {
+                sh '/opt/apache-maven-3.9.6/bin/mvn clean package'
+                echo 'buildup is ready'
+            }
+        }
+        stage('test') {
+            steps {
+               sh ''' /opt/apache-maven-3.9.6/bin/mvn sonar:sonar \
+                     -Dsonar.projectKey=student-app \
+                     -Dsonar.host.url=http://44.201.211.235:9000 \
+                     -Dsonar.login=6a120818611d31dc2054bc3c4b904c226fdceb48 '''
+
+            }
+        }
+        stage('quality test') {
+            steps {
+                echo 'quality check is done'
+            }
+        }
+        stage('deploy') {
+            steps {
+                echo 'deployment is done'
+            }
+        }
+    }
+}
